6682c49082c5b7610fb19e320ecca7c8e2aea4e3
usman <usmankhan14700@gmail.com>
2026-07-15 12:32:23 +0500

    Remove conflicting admin API routes; use 3 serverless functions only

--- api/admin/[...slug].js
+++
@@ -1,4 +0,0 @@
-import { createVercelApiHandler, vercelFunctionConfig } from '../../server/vercelHandler.js';
-
-export default createVercelApiHandler();
-export const config = vercelFunctionConfig;
